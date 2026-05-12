c0731aeabb735aaa07296cbc3424ed6e5f7a5a5f
nachu <paredesignacio2001@gmail.com>
2026-05-11 21:01:00 -0400

    feath: creación del model y configuración inicial de MCVC auth

---
+++ auth/src/main/java/com/GameHub/models/Cuenta.java
@@ -0,0 +1,47 @@
+package com.GameHub.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="cuenta")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Cuenta {
+
+    @NotBlank(message = "El nombre de cuenta es obligatorio")
+    @Column(unique = true)
+    private String nombreCuenta;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cuenta_id")
+    private Long Id;
+
+    @NotBlank(message = "El campo de correo no puede ser vacio")
+    @Email(message = "El campo de correo tiene que tener el formato de correo")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "La constraseña es obligatorio y no puede estar vacia")
+    private String passwordHash;
+
+    @NotBlank(message = "El rol es obligatorio")
+    private String rol;
+
+    @NotBlank(message = "El estado es obligatorio")
+    private String estado; //
+
+    @Column(name = "fecha_creacion", updatable = false)
+    private LocalDateTime fechaCreacion;
+
+}
