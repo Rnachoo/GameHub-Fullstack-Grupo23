c935fee3cc8c64d0c4aa7535fd4abe9b44068ea9
nachu <paredesignacio2001@gmail.com>
2026-05-12 21:17:01 -0400

    Feath: Creación temprana de MSVC user

--- msvc_user/src/main/java/com/GameHub/MsvcUser.java
+++ msvc_user/src/main/java/com/GameHub/MsvcUser.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class MsvcAuth {
+public class MsvcUser {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsvcAuth.class, args);
+        SpringApplication.run(MsvcUser.class, args);
     }
 
 }
--- msvc_user/src/main/java/com/GameHub/models/User.java
+++ msvc_user/src/main/java/com/GameHub/models/User.java
@@ -3,42 +3,41 @@ package com.GameHub.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name="cuenta")
+@Table(name = "user")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-public class Auth {
+public class User {
 
-    @NotBlank(message = "El nombre de cuenta es obligatorio")
-    @Column(unique = true)
-    private String nombreCuenta;
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cuenta_id")
-    private Long Id;
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    @Column(unique = true, nullable = false)
+    private String nombreUser;
 
     @NotBlank(message = "El campo de correo no puede ser vacio")
     @Email(message = "El campo de correo tiene que tener el formato de correo")
     @Column(nullable = false, unique = true)
     private String email;
 
-    @NotBlank(message = "La constraseña es obligatorio y no puede estar vacia")
-    private String passwordHash;
-
+    @NotBlank(message = "El número de telefono es obligatorio")
+    @Column(unique = true)
+    private String telefono;
 
     private String rol;
     private String estado;
-    @Column(name = "fecha_creacion", updatable = false)
-    private LocalDateTime fechaCreacion;
 
 }
