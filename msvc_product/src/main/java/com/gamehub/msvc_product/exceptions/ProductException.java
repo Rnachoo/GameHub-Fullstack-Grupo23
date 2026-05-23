0c36a6eb175a92f983737ae2f9bc68f52796fff2
nachu <paredesignacio2001@gmail.com>
2026-05-22 23:15:23 -0400

    fix: se elimino msvc product creado erróneamente

--- msvc_product/src/main/java/com/gamehub/msvc_product/exceptions/ProductException.java
+++
@@ -1,7 +0,0 @@
-package com.gamehub.msvc_product.exceptions;
-
-public class ProductException extends RuntimeException {
-    public ProductException(String message) {
-        super(message);
-    }
-}
