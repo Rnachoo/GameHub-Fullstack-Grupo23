72029c213196eca3a19524923eea3db8d6db6c9a
nachu <paredesignacio2001@gmail.com>
2026-05-23 22:02:01 -0400

    fix: se arreglarón los puertos de conexion de los msvc actuales

--- msvc_promotion/src/main/java/com/GameHub/clients/CategoryClient.java
+++ msvc_promotion/src/main/java/com/GameHub/clients/CategoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-category", url = "localhost:8001/api/categories")
+@FeignClient(name = "msvc-category", url = "localhost:8004/api/categories")
 public interface CategoryClient {
     @GetMapping("/id/{id}")
     CategoryDTO getCategoryById(@PathVariable("id") Long id);
