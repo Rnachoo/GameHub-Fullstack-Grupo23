5504b5153a3e5e97dfca1baee8a5512d739fcafb
Facundo Benítez <facupoxy@gmail.com>
2026-05-25 14:10:32 -0400

    feat: se crea clients con su contenido: CategoryFeignClient Y  dtos: CategoryClientDTO.

---
+++ msvc_product/src/main/java/com/GameHub/clients/dtos/CategoryClientDTO.java
@@ -0,0 +1,16 @@
+package com.GameHub.clients.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class CategoryClientDTO {
+    private Long id;
+    private String nombreCategory;
+    private String estado;
+}
