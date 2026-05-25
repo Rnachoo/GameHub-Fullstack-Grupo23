c8c1ca3774a704227bc5b7ead125e01743729d27
Facundo Benítez <facupoxy@gmail.com>
2026-05-25 15:09:07 -0400

    fix: se añaden cambios menores

---
+++ msvc_shipping/src/test/java/com/GameHub/MsvcShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcShippingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
