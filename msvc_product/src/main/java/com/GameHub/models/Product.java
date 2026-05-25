f1a0bfaf36e9c1afea8953610f683a44b562803e
Facundo Benítez <facupoxy@gmail.com>
2026-05-25 13:53:20 -0400

    feat: se crea model y su contenido: Product.java

---
+++ msvc_product/src/main/java/com/GameHub/models/Product.java
@@ -0,0 +1,42 @@
+package com.GameHub.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Productos")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String marca;
+
+    private String modelo;
+
+    @Column(nullable = false)
+    private BigDecimal precio;
+
+    @Column(name =  "categoria_id", nullable = false)
+    private Long categoriaId;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private boolean estado;
+
+}
